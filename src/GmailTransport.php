d6be8e6036c00f455c88a3ab4cdb0ebdc27b9c46
片岡高志 <web@ad5.jp>
2024-08-08 07:05:50 +0900

    for modern google api client

--- src/GmailTransport.php
+++ src/GmailTransport.php
@@ -2,9 +2,9 @@
 
 namespace AD5jp\LaravelGmail;
 
-use Google_Client;
-use Google_Service_Gmail;
-use Google_Service_Gmail_Message;
+use Google\Client;
+use Google\Service\Gmail;
+use Google\Service\Gmail\Message;
 use Illuminate\Mail\Transport\Transport;
 use Swift_Mime_SimpleMessage;
 
@@ -14,15 +14,15 @@ class GmailTransport extends Transport
     {
         $this->beforeSendPerformed($message);
 
-        $client = new Google_Client();
+        $client = new Client();
         $client->setApplicationName(config('app.name'));
         $client->setAuthConfig(base_path(config('gmail.service_account_key')));
-        $client->setScopes([Google_Service_Gmail::GMAIL_SEND]);
+        $client->setScopes([Gmail::GMAIL_SEND]);
         $client->setSubject(config('gmail.from_address'));
 
-        $service = new Google_Service_Gmail($client);
+        $service = new Gmail($client);
 
-        $gmail_message = new Google_Service_Gmail_Message();
+        $gmail_message = new Message();
         $gmail_message->setRaw(base64_encode($message->toString()));
 
         $service->users_messages->send(config('gmail.from_address'), $gmail_message);

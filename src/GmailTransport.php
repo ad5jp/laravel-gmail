9e578cafc4aa4f79ab9d13337f2ffab417287f46
片岡高志 <web@ad5.jp>
2024-08-08 11:18:28 +0900

    for laravel 9 or higher: SwiftMailer to SymfonyMailer

--- src/GmailTransport.php
+++ src/GmailTransport.php
@@ -5,30 +5,36 @@ namespace AD5jp\LaravelGmail;
 use Google\Client;
 use Google\Service\Gmail;
 use Google\Service\Gmail\Message;
-use Illuminate\Mail\Transport\Transport;
-use Swift_Mime_SimpleMessage;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\AbstractTransport;
 
-class GmailTransport extends Transport
+class GmailTransport extends AbstractTransport
 {
-    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
-    {
-        $this->beforeSendPerformed($message);
+    protected $client;
 
+    public function __construct()
+    {
         $client = new Client();
         $client->setApplicationName(config('app.name'));
         $client->setAuthConfig(base_path(config('gmail.service_account_key')));
         $client->setScopes([Gmail::GMAIL_SEND]);
         $client->setSubject(config('gmail.from_address'));
 
-        $service = new Gmail($client);
+        parent::__construct();
+    }
+
+    protected function doSend(SentMessage $message): void
+    {
+        $service = new Gmail($this->client);
 
         $gmail_message = new Message();
-        $gmail_message->setRaw(base64_encode($message->toString()));
+        $gmail_message->setRaw(base64_encode($message->getOriginalMessage()->toString()));
 
         $service->users_messages->send(config('gmail.from_address'), $gmail_message);
+    }
 
-        $this->sendPerformed($message);
-
-        return $this->numberOfRecipients($message);
+    public function __toString(): string
+    {
+        return 'gmail';
     }
 }

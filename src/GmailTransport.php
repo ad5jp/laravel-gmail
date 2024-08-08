36d2137beb535269bf65b0c81849e612486befc7
片岡高志 <web@ad5.jp>
2024-08-08 12:54:54 +0900

    fix for MailManager

--- src/GmailTransport.php
+++ src/GmailTransport.php
@@ -10,22 +10,30 @@ use Swift_Mime_SimpleMessage;
 
 class GmailTransport extends Transport
 {
+    protected $config;
+    protected $client;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+
+        $this->client = new Client();
+        $this->client->setApplicationName(config('app.name'));
+        $this->client->setAuthConfig(base_path($this->config['service_account_key']));
+        $this->client->setScopes([Gmail::GMAIL_SEND]);
+        $this->client->setSubject($this->config['from_address']);
+    }
+
     public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
         $this->beforeSendPerformed($message);
 
-        $client = new Client();
-        $client->setApplicationName(config('app.name'));
-        $client->setAuthConfig(base_path(config('gmail.service_account_key')));
-        $client->setScopes([Gmail::GMAIL_SEND]);
-        $client->setSubject(config('gmail.from_address'));
-
-        $service = new Gmail($client);
+        $service = new Gmail($this->client);
 
         $gmail_message = new Message();
         $gmail_message->setRaw(base64_encode($message->toString()));
 
-        $service->users_messages->send(config('gmail.from_address'), $gmail_message);
+        $service->users_messages->send($this->config['from_address'], $gmail_message);
 
         $this->sendPerformed($message);
 
--- src/config/gmail.php
+++ src/config/gmail.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'transport' => 'gmail',
     'from_address' => env('GMAIL_FROM_ADDRESS'),
     'service_account_key' => env('GMAIL_SERVICE_ACCOUNT_KEY'),
 ];
